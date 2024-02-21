2267c9e15f38b48453cc06bc6834775d684a74ab
DanielOrlandoVi <dovillescasm@udistrital.edu.co>
2024-02-21 10:42:04 -0500

    Se corrige apartado de interfaz

--- src/Controlador/Controlador.java
+++ src/Controlador/Controlador.java
@@ -9,21 +9,47 @@ public class Controlador {
         
         Singleton impresora = Singleton.getImpresora();
         
+        
+        Controlador menu = new Controlador();
         while(1 != 0){
-            
-            int opcion = Integer.parseInt(JOptionPane.showInputDialog("Sistema de gestion de impresora" + 
-                    "\n" + "Elija una opción: " + "\n" +
-                    "1. Consultar datos impresora" + 
-                    "\n" + "2. Salir"));
-            
-            if (opcion == 1){
-                JOptionPane.showMessageDialog(null, impresora.getDatosImpresora());
-            }
-            if (opcion == 2){
-                System.exit(0);
-            }
+            menu.mostrar_Menu();
+            menu.mostrardatos();
         }
-
     }
     
+    
+    //LOGICA DE INTERFAZ GRÁFICA
+    int opcion;
+    
+    public int mostrar_Menu(){
+        String opcion_texto = JOptionPane.showInputDialog("Sistema de gestion de impresora" + 
+                    "\n" + "Elija una opción: " + "\n" +
+                    "1. Consultar datos impresora" + 
+                    "\n" + "2. Salir");
+        
+        if (opcion_texto == null) {
+            System.exit(0);   
+        }
+        try {
+        opcion = Integer.parseInt(opcion_texto);
+        }
+        catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Error: Ingrese un número válido.", "Error", JOptionPane.ERROR_MESSAGE);
+            return mostrar_Menu();
+        }
+        return opcion;
+    }
+    
+    
+    public void mostrardatos(){
+    	Singleton impresora = Singleton.getImpresora();
+
+        if (opcion == 1){
+                JOptionPane.showMessageDialog(null, impresora.getDatosImpresora());
+            }
+        if (opcion == 2){
+                System.exit(0);     
+            }
+    }
 }
+
